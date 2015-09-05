af09135fdddcb7e226d1373e293b5bac18e76e49
Tom Fuertes <tomfuertes@gmail.com>
2015-09-05 18:00:08 -0400

    fx license && rebuild

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+      ' Licensed WTFPL */\n',
     // Task configuration.
     concat: {
       options: {
