675162991d1ed67bbfc27c22336b34e1e9506ebf
Tom Fuertes <tomfuertes@gmail.com>
2015-09-05 18:50:41 -0400

    0.1.0

--- dist/bootstrap-analytics.v0.1.0.js
+++ dist/bootstrap-analytics.v0.1.0.js
@@ -1,4 +1,4 @@
-/*! bootstrap-analytics - v0.0.4 - 2015-09-05
+/*! bootstrap-analytics - v0.1.0 - 2015-09-05
 * https://github.com/tomfuertes/bootstrap-analytics
 * Copyright (c) 2015 Tom Fuertes <tomfuertes@gmail.com>; Licensed WTFPL */
 (function ($) {
